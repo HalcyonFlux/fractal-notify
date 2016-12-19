19d6d5f02bf5e022c662bc5c8dfa7fe818cba827
Mindaugas Vaitekunas <mindaugas@halcyonflux.com>
2016-12-19 12:16:56 +0000

    Updated doc.go

--- src/fractal/notify/doc.go
+++ src/fractal/notify/doc.go
@@ -1,18 +1,20 @@
 // Package notify is a simplistic notification/logging service used in HalcyonFlux/fractal.
 //
-// Use notify.Send, or a function created by notifier.PersonalizeSend, to send
-// and log notifications.
-// notify.Send understands string, notify.Notification types and the errors.error
-// interface.
+// Use a function created by notifier.Sender or notifier.Failure to send and
+// log notifications. notify.Sender understands string, notify.notification types
+// and the errors.error interface.
 //
-// Use notifier.Loop() either sequentially or in a goroutine to run the service.
+// Use notifier.Run() either sequentially or in a goroutine to run the service.
 //
 // General advices on using notify:
 // * even though you can have several notifiers pointing to different logfiles,
 // use as little notifiers as possible to simplify log-analysis.
 //
-// * use notifier.PersonalizeSend to simplify notifications
+// * use notifier.Sender and notifier.Failure to create personalized notification
+// senders.
 //
-// * use notify.Send (or a personalized function) in case you want to both
-// display/log the error and return an object implementing errors.error interface.
+// * use os.Stdout as an endpoint to view logs and messages in debug mode.
+//
+// * connect notify to remote services by suplying an instance of *os.File (e.g.
+// a write-end of os.Pipe. Then use the read-end to get notifications).
 package notify
